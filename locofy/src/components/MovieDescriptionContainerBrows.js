eed57d58d2c0550de2806f8fecd6d7cb27b21bb7
Vedanta Neogi <vedantaneogi@gmail.com>
2023-04-17 14:59:54 +0530

    Co-authored-by: VinayTayal <VinayTayal@users.noreply.github.com>

--- locofy/src/components/MovieDescriptionContainerBrows.js
+++ locofy/src/components/MovieDescriptionContainerBrows.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import { useNavigate } from "react-router-dom";
 import "./MovieDescriptionContainerBrows.css";
 
 const MovieDescriptionContainerBrows = ({
   productId,
-  onMovieDescriptionBrowsePageContainerClick,
+  movie,
 }) => {
+
+  const navigate = useNavigate();
+
+  const onMovieDescriptionBrowsePageContainerClick = useCallback(() => {
+    navigate("/movieout", {state: {movie} });
+  }, [navigate]);
+
   return (
     <div
-      className="moviedescription-browsepage"
-      onClick={onMovieDescriptionBrowsePageContainerClick}
+      className="moviedescription-browsepage" onClick={onMovieDescriptionBrowsePageContainerClick}
     >
       <div className="picture6">
-        <img className="joker-icon5" alt="" src="/joker2@2x.png" />
+        <img className="joker-icon5" alt="" src={movie.poster} />
       </div>
       <div className="right11">
         <div className="header2">
           <div className="title19">Title</div>
-          <div className="year4">(Year)</div>
+          <div className="year4">({movie.release_date.getFullYear()})</div>
         </div>
         <div className="subheader1">
           <div className="rating24">
-            <div className="xy1014">x.y/10</div>
+            <div className="xy1014">{movie.rating}/10</div>
             <img className="vector-icon44" alt="" src={productId} />
           </div>
           <div className="duration2">
-            <p className="duration3">Duration</p>
+            <p className="duration3">{movie.duration}</p>
           </div>
         </div>
         <div className="body23">
-          <div className="credits3">{`Cast: John Doe, Jane Doe, John Doe `}</div>
+          <div className="credits3">Cast: {movie.cast.join(",")}</div>
           <div className="genres11">
-            <div className="genre215">
-              <div className="genre13">Genre</div>
+          {movie.genres.map((genre, index) => (
+            <div key={index} className="genre215">
+              <div className="genre13">{genre}</div>
             </div>
-            <div className="genre215">
-              <div className="genre13">Genre</div>
-            </div>
-            <div className="genre215">
-              <div className="genre13">Genre</div>
-            </div>
-          </div>
+          ))}
+        </div>
+
         </div>
       </div>
     </div>
